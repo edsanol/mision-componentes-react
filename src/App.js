68ba8c6565461c6b6d36e48befea564c0c3d497e
Edinael <edsanol99@gmail.com>
2022-05-08 18:18:59 -0500

    Add mision-components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const Title = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+const Subtitle = ({counter}) => {
+  return (
+    <h2>{counter}</h2>
+  )
+}
+const Button = ({text, handleClick}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
 
 function App() {
+  const [ counter, setCounter ] = useState(0)
+
+  const increment = () => setCounter(counter + 1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title text={"Contador"}/>
+      <Button text={"Botón"} handleClick={increment}/>
+      <Subtitle counter={counter}/>
     </div>
   );
 }
